e0961ef0ebb8b2a91ad0331d6032cf2689b651e5
ray-calvo <46980899+ray-calvo@users.noreply.github.com>
2019-09-02 18:54:36 -0500

    Muchos cambios, lo mas significativo es la navigation

---
+++ MyVet.Common/Models/TokenRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVet.Common.Models
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+}
+
