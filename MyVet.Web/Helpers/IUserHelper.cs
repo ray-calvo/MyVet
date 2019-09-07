61531fdecd148dd8cace62825efcecd914ea4967
ray-calvo <raycalvo@hotmail.com>
2019-09-06 23:23:46 -0500

    Reset Password

--- MyVet.Web/Helpers/IUserHelper.cs
+++ MyVet.Web/Helpers/IUserHelper.cs
@@ -34,6 +34,11 @@ namespace MyVet.Web.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+
     }
 }
   
